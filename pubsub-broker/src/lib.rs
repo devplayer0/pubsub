61f475ddabf8803a61cf3bfd731515b90a6eac1c
Jack O'Sullivan <jackos1998@gmail.com>
2018-09-25 14:58:13 +0100

    Refactor broker into struct

--- pubsub-broker/src/lib.rs
+++ pubsub-broker/src/lib.rs
@@ -1,11 +1,12 @@
 use std::error::Error as StdError;
 use std::any::Any;
+use std::mem;
 use std::time::Duration;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::io;
-use std::net::UdpSocket;
+use std::net::{SocketAddr, UdpSocket};
 
 #[macro_use]
 extern crate quick_error;
@@ -19,55 +20,91 @@ use config::Config;
 
 quick_error! {
     #[derive(Debug)]
-    enum Error {
+    pub enum Error {
         ThreadPanic(err: Box<dyn Any + Send>) {
-            display(e) -> ("thread panicked: {:?}", err)
+            display("thread panicked: {:?}", err)
         }
+        Io(err: io::Error) {
+            from()
+            display("io error: {}", err)
+            description(err.description())
+            cause(err)
+        }
+    }
+}
+
+pub struct Broker {
+    running: Arc<AtomicBool>,
+    socket_threads: Vec<thread::JoinHandle<Result<(), Error>>>,
+}
+impl Broker {
+    pub fn bind<'a, I>(addrs: I) -> Result<Broker, io::Error>
+    where I: IntoIterator<Item = &'a SocketAddr>
+    {
+        let running = Arc::new(AtomicBool::new(true));
+        let addrs = addrs.into_iter();
+        let (l, _) = addrs.size_hint();
+        let mut socket_threads = Vec::with_capacity(l);
+        for addr in addrs {
+            info!("binding on {}...", addr);
+
+            let socket = UdpSocket::bind(addr)?;
+            socket.set_read_timeout(Some(Duration::from_millis(250)))?;
+
+            let r = Arc::clone(&running);
+            socket_threads.push(thread::spawn(move || {
+                let mut buf = [0; 65536];
+                while r.load(Ordering::SeqCst) {
+                    let (size, src) = match socket.recv_from(&mut buf) {
+                        Ok((size, src)) => (size, src),
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+                        Err(e) => return Err(Error::Io(e))
+                    };
+
+                    debug!("received packet of size {} bytes from {}!", size, src);
+                }
+
+                Ok(())
+            }));
+        }
+
+        Ok(Broker {
+            running,
+            socket_threads,
+        })
+    }
+
+    pub fn running(&self) -> bool {
+        self.running.load(Ordering::SeqCst)
+    }
+    pub fn join(&mut self) -> Result<(), Error> {
+        let mut threads = Vec::with_capacity(0);
+        mem::swap(&mut threads, &mut self.socket_threads);
+
+        for thread in threads.into_iter() {
+            thread.join().map_err(|e| Error::ThreadPanic(Box::new(e)))??;
+        }
+
+        Ok(())
+    }
+    pub fn stop(&self) {
+        self.running.store(false, Ordering::SeqCst);
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn StdError>> {
     info!("starting broker");
 
-    let running = Arc::new(AtomicBool::new(true));
-    let mut threads = Vec::with_capacity(config.bind_addrs().len());
-    for addr in config.bind_addrs().iter().cloned() {
-        info!("binding on {}...", addr);
-
-        let socket = UdpSocket::bind(addr)?;
-        socket.set_read_timeout(Some(Duration::from_millis(250)))?;
-
-        let r = Arc::clone(&running);
-        threads.push(thread::spawn(move || {
-            let mut buf = [0; 65536];
-            while r.load(Ordering::SeqCst) {
-                let (size, src) = match socket.recv_from(&mut buf) {
-                    Ok((size, src)) => (size, src),
-                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
-                    Err(e) => {
-                        error!("failed to recv from socket bound to {}: {}", addr, e);
-                        r.store(false, Ordering::SeqCst);
-                        return;
-                    }
-                };
-
-                debug!("received packet of size {} bytes from {}!", size, src);
-            }
-        }));
-    }
-
+    let broker = Arc::new(Mutex::new(Broker::bind(config.bind_addrs())?));
     {
-        let r = Arc::clone(&running);
+        let b = Arc::clone(&broker);
         ctrlc::set_handler(move || {
             info!("shutting down...");
-            r.store(false, Ordering::SeqCst);
+            b.lock().unwrap().stop();
         })?;
     }
 
-    for thread in threads {
-        thread.join().map_err(|e| Error::ThreadPanic(Box::new(e)))?
-    }
-
+    broker.lock().unwrap().join()?;
     Ok(())
 }
 
